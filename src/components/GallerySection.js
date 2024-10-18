53b2e287ca1116eba8e404bf8b78230086de1a9d
szymonstankiewcz <szymon@tlt.local>
2024-10-18 18:15:43 +0400

    Gallery img wrapping

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -2,10 +2,14 @@ import React from "react";
 import styles from './GallerySection.module.css';
 
 
-import {MasonryPhotoAlbum} from "react-photo-album";
+import {MasonryPhotoAlbum, RowsPhotoAlbum} from "react-photo-album";
 import "react-photo-album/masonry.css";
 import {Lightbox} from "yet-another-react-lightbox"
 import "yet-another-react-lightbox/styles.css"
+
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
+import {DndContext} from '@dnd-kit/core';
 
 
 const GallerySection = ({images, name}) => {
@@ -15,14 +19,28 @@ const GallerySection = ({images, name}) => {
 
     const [index, setIndex] = React.useState(-1);
 
+    function wrapImage(props, index) {
+        return <Draggable id={"draggable-photo-" + index} wrappedProps={props}/>
+    }
+
+    function handleDragEnd() {
+    }
+
     return (
         <>
             <h1 className={styles["gallery-section__name"]}>{name}</h1>
-            <MasonryPhotoAlbum
-                photos={photos}
-                columns={3}
-                onClick={({index: current}) => setIndex(current)}
-            />
+            <DndContext onDragEnd={handleDragEnd}>
+                <MasonryPhotoAlbum
+                    photos={photos}
+                    columns={3}
+                    render={{
+                        wrapper: (props, {index, photo}) => wrapImage(props, index),
+                    }}
+                    /*
+                    onClick={({index: current}) => setIndex(current)}
+                    */
+                />
+            </DndContext>
 
             <Lightbox
                 index={index}
@@ -32,6 +50,23 @@ const GallerySection = ({images, name}) => {
             />
         </>
     )
+
+
 };
+
+function Draggable(props) {
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: props.id
+    });
+    const style = {
+        transform: CSS.Translate.toString(transform),
+    };
+
+    return (
+        <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+            <div {...props.wrappedProps} />
+        </div>
+    );
+}
 
 export default GallerySection;
